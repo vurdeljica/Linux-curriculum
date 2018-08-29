8fa956970bdfe608d9e20d0dad9f9a060fe18a99
igvu <igvu@debian.dev>
2018-08-29 10:42:41 +0200

    Checkpatch validated

--- Hello.c
+++ Hello.c
@@ -1,21 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0+
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Igor Vurdelja");
 MODULE_DESCRIPTION("A simple Linux driver");
 MODULE_VERSION("0.1");
 
 static int __init hello_init(void)
 {
-    printk(KERN_INFO "Hello module is loaded!");
-    return 0;
+pr_info("Hello module is loaded!");
+return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    printk(KERN_INFO "Hello module is unloaded!");
+pr_info("Hello module is unloaded!");
 }
 
 module_init(hello_init);
